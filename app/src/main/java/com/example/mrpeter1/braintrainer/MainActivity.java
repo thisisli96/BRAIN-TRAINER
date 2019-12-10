33629b7cc4fde8aa6201bf389c8bc1ab9b307eba
muh ikhsan <thisisli96@yahoo.com>
2019-12-10 23:33:16 +0700

    step 2 membuat angka random pada pertanyaan

--- app/src/main/java/com/example/mrpeter1/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/mrpeter1/braintrainer/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.mrpeter1.braintrainer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button goevent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        goevent = findViewById(R.id.goevent);
+
+        goevent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotosuccesstiket = new Intent(MainActivity.this,mainevent.class); // kenapa biki variabel gotosign?
+                startActivity(gotosuccesstiket); // saat klik dia akan keSignAct activity
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mrpeter1/braintrainer/mainevent.java
+++ app/src/main/java/com/example/mrpeter1/braintrainer/mainevent.java
@@ -3,12 +3,22 @@ package com.example.mrpeter1.braintrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class mainevent extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainevent);
+        textView = findViewById(R.id.textView);
+
+        Random rand = new Random();
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+        textView.setText(Integer.toString(a) + " + " + Integer.toString(b));
+
     }
 }
